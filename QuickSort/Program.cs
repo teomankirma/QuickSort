aef12758d4fc5758f19a84e4f6277a7f63274c4e
teomankirma <83593653+teomankirma@users.noreply.github.com>
2022-04-16 19:52:17 +0300

    Quick Sort

---
+++ QuickSort/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace QuickSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 3, 7, 25, 87, -30, -23, 134, 60, -147 };
+
+            QuickSort.Sort(numbers);
+            Console.Write("Quick Sort");
+            Console.WriteLine(" ");
+            Console.WriteLine(string.Join(" | ", numbers));
+
+        }
+    }
+}
